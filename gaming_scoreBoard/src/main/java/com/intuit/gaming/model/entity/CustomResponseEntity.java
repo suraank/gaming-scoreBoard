cd07a16b2dee189ad5a946c8580269d09db3fb5e
suraank <surabhichaurasia29@gmail.com>
2024-07-11 01:00:27 +0530

    Added tests

--- gaming_scoreBoard/src/main/java/com/intuit/gaming/model/entity/CustomResponseEntity.java
+++ gaming_scoreBoard/src/main/java/com/intuit/gaming/model/entity/CustomResponseEntity.java
@@ -1,11 +1,3 @@
-/*-Author------------------------------------
-*- bibhas.abhishek@gmail.com
-*- uber-onboarding: CustomResponseEntity
-*- 26 Nov 2021 12:44 AM
----Made with <3 in Delhi,India---------------
----Details-----------------------------------
-*- Links:
--------------------------------------------*/
 
 package com.intuit.gaming.model.entity;
 
